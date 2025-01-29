44031779ce7b8a780ad729fefb640b211700cf5b
tergelitu <131893444+tergelitu@users.noreply.github.com>
2025-01-29 19:58:18 +0800

    Update setup.py to fix license field issue

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='financial_rsi_bot',
     version='1.0.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A bot to analyze and visualize XAU/USD price data using the RSI indicator',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/repository-name',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
@@ -13,7 +22,14 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'run-analysis=main:main',
+            'run-analysis=teddy.main:main',
         ],
     },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    license='MIT',
 )
