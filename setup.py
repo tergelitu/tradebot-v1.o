7c47ecb5a01eea9c6e1e09a9e72e010a9e3adb20
tergelitu <131893444+tergelitu@users.noreply.github.com>
2025-01-30 15:26:06 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setup(
     name='financial_rsi_bot',
     version='1.0.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A bot to analyze and visualize XAU/USD price data using the RSI indicator',
+    author='Tergel Munkhbaatar',
+    author_email='tergelitu@gmail.com',
+    description='A bot to analyze and visualize price data using the RSI indicator',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/repository-name',
+    url='https://github.com/tergelitu/tradebot.v1.o',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
